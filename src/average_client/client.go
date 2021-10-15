a4c6ae7affbd38ccf23f3d7b090dd4e42fa06bd6
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-10-15 17:19:10 -0400

    feat: server request

--- src/average_client/client.go
+++ src/average_client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"computeAverage/src/average_proto"
+	"context"
 	"fmt"
 	"log"
 
@@ -19,4 +20,26 @@ func main() {
 	if c != nil {
 		fmt.Println("Conexão com servidor criada com sucesso!")
 	}
+	doClientStreaming(c)
+}
+
+func doClientStreaming(c average_proto.CalculateServiceClient) {
+	fmt.Println("Consulta media")
+	stream, err := c.ComputeAverage(context.Background())
+	if err != nil {
+		log.Fatalf("Erro na requisição: %v \n", err)
+	}
+	numbers := []int32{1, 2, 3, 4, 5, 6}
+	for _, num := range numbers {
+		//enviando dados ao servidor
+		stream.Send(&average_proto.ComputeAverageRequest{
+			Number: num,
+		})
+	}
+	//fechando stream e obtendo retorno
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Erro na resposta: %v\n", err)
+	}
+	fmt.Printf("A média é: %v\n", res.GetAverage())
 }
