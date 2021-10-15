fe1717efd9359bc2ab7a2baadb66b8937c08d084
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-10-15 17:18:43 -0400

    feat: get mean

--- src/average_server/server.go
+++ src/average_server/server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"computeAverage/src/average_proto"
+	"fmt"
+	"io"
 	"log"
 	"net"
 
@@ -22,5 +24,27 @@ func main() {
 	average_proto.RegisterCalculateServiceServer(s, &server{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Falha ao setar server: %v\n", err)
+	}
+}
+
+func (*server) ComputeAverage(stream average_proto.CalculateService_ComputeAverageServer) error {
+	fmt.Println("Calcula média:")
+	sum := int32(0)
+	count := 0
+	//obter entradas do usuario
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			average := float64(sum) / float64(count)
+			//ao chegar no final do arquivo retorne o resultado(response)
+			return stream.SendAndClose(&average_proto.ComputeAverageResponse{
+				Average: average,
+			})
+		}
+		if err != nil {
+			log.Fatalf("Erro de entrada: %v\n", err)
+		}
+		sum += req.GetNumber()
+		count++
 	}
 }
